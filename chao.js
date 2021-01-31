4656aae14c142f963d88fcab06075f20ddad9d70
Allen Luce <allen.luce@prometheanworld.com>
2021-01-30 18:29:12 -0800

    More refactoring
    Remove the odd array-selection syntax and instead use the select
    argument to choose the position of the alphabets to be operated
    on. This is slightly more clear and a good bit shorter.
    
    Put rotate on String directly. Remove the modulus, it'll never
    actually do anything (as this is only called on the string the length
    is derived from if it's not 1). If rotateAmount is zero, do the
    rotation anyways to save a line of code.
    
    Get rid of the charAt/indexOf methods and have the caller just use the
    ones on data directly. Exchange 10 added characters for 8 removed
    lines. Demeter be damned.

--- chao.js
+++ chao.js
@@ -1,57 +1,39 @@
 'use strict'
+
+String.prototype.rotate = function (rotateAmount) {
+  return this.slice(rotateAmount) + this.slice(0, rotateAmount)
+}
+
 class Alphabet {
   constructor (characters) {
     this.data = characters
     this.nadirIndex = Math.floor(characters.length / 2)
   }
 
-  charAt (i) {
-    return this.data.charAt(i)
-  }
-
-  indexOf (c) {
-    return this.data.indexOf(c)
-  }
-
-  rotate (str, n) {
-    const rotateAmount = n % str.length
-    if (rotateAmount < 1) return str
-    return str.slice(rotateAmount) + str.slice(0, rotateAmount)
-  }
-
   permute (newZenith, skipAmount) {
-    let permutedCharacters = this.rotate(this.data, this.data.indexOf(newZenith))
-    if (skipAmount === 2) { permutedCharacters = this.rotate(permutedCharacters, 1) }
+    let permutedCharacters = this.data.rotate(this.data.indexOf(newZenith))
+    if (skipAmount === 2) permutedCharacters = permutedCharacters.rotate(1)
     this.data = permutedCharacters.slice(0, skipAmount) +
-          this.rotate(permutedCharacters.slice(skipAmount, this.nadirIndex + 1), 1) +
-          permutedCharacters.slice(this.nadirIndex + 1)
+      permutedCharacters.slice(skipAmount, this.nadirIndex + 1).rotate(1) +
+      permutedCharacters.slice(this.nadirIndex + 1)
   }
 }
 
 module.exports = class Chao {
   constructor (ciphertextAlphabet, plaintextAlphabet) {
     this.reinitialize = function () {
-      this.ciphertextAlphabet = new Alphabet(ciphertextAlphabet)
-      this.plaintextAlphabet = new Alphabet(plaintextAlphabet)
+      this.alphabets = [new Alphabet(ciphertextAlphabet), new Alphabet(plaintextAlphabet)]
     }
-  }
-
-  ciphertextLetterFor (plaintextLetter) {
-    return this.ciphertextAlphabet.charAt(this.plaintextAlphabet.indexOf(plaintextLetter))
-  }
-
-  plaintextLetterFor (ciphertextLetter) {
-    return this.plaintextAlphabet.charAt(this.ciphertextAlphabet.indexOf(ciphertextLetter))
   }
 
   process (text, select) {
     this.reinitialize()
     return text.split('').map(c => {
-      const lastConvertedLetter = [this.ciphertextLetterFor, this.plaintextLetterFor][select].call(this, c)
-      // Only convert characters in the alphabet.
+      const lastConvertedLetter = this.alphabets[select].data.charAt(this.alphabets[1-select].data.indexOf(c))
+      // Only convert characters in the source alphabet.
       if (lastConvertedLetter === '') return c
-      this.ciphertextAlphabet.permute([lastConvertedLetter, c][select], 1)
-      this.plaintextAlphabet.permute([c, lastConvertedLetter][select], 2)
+      this.alphabets[select].permute(lastConvertedLetter, 1+select)
+      this.alphabets[1-select].permute(c, 2-select)
       return lastConvertedLetter
     }).join('')
   }
